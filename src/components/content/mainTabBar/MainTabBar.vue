2ffa24df45b31228ced42bccc760ecd0026cfed9
coderzzca <1370054696@qq.com>
2022-03-20 15:57:45 +0800

    基本配置

---
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+        <tab-bar>
+      <tab-bar-item path="/home" activeColor="deepPink">
+        <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="deepPink">
+        <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/shopcart" activeColor="deepPink">
+        <img slot="item-icon" src="~assets/img/tabbar/shopcart.svg" alt="">
+          <img slot="item-icon-active" src="~assets/img/tabbar/shopcart_active.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="deepPink">
+        <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar> 
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TabBar from '../tabbar/TabBar.vue'
+import TabBarItem from '../tabbar/TabBarItem.vue'
+export default {
+  components:{
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+
+</style>
